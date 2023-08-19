0e52678e43b23cd00c97c187f45156faed4cb60b
DanielSudiOdeny <daniel.odeny@student.moringaschool.com>
2023-08-19 11:23:56 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens = [n for n in num_list if n % 2 == 0]
+    return evens
+
+
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
 
 def make_exclamation(sentence_list):
-    pass
+    sentences = [f"{s}!" for s in sentence_list]
+    return sentences
+
+
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
